feb2f046bfa1c41a816f27f089ce496a3104a04c
hyeji <gpwl9610@gmail.com>
2021-03-04 16:42:48 +0900

    first add

---
+++ src/main/java/com/jsb/haf/HafApplication.java
@@ -0,0 +1,13 @@
+package com.jsb.haf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsb/haf/HafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsb.haf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
