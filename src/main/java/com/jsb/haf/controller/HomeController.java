1bc2c99211af2ece7612f7d2f84e69027e926f52
seonggyu-lee <seonggyu7478@daum.net>
2021-03-07 16:10:07 +0900

    mySQL 연동 확인

--- src/main/java/com/jsb/haf/controller/HomeController.java
+++ src/main/java/com/jsb/haf/controller/HomeController.java
@@ -1,37 +1,28 @@
 package com.jsb.haf.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Optional;
+
+
+
+@RequestMapping("/api")
 @RestController     // → @ResponseBody + @Controller
-@RequestMapping("/hi")
 public class HomeController {
 
     //안되네
     // 3/5에 참고 : https://reiphiel.tistory.com/entry/spring-security-csrf
     // 왜 postmapping이 에러나는지.
-    @PostMapping("/postTest")
-    public String postTest(){
-        return "postTest";
-    }
 
-    @GetMapping("/post")
-    public String post() {
-        //return "board/post.html";
-        return "post";
-    }
 
     @GetMapping(value = "/why")
     public ModelAndView why(){
-        ModelAndView mv = new ModelAndView("post");
+        ModelAndView mv = new ModelAndView("postTest");
         return mv;
     }
 
-    @GetMapping(value = "/jji")
-    public ModelAndView home() {
-        ModelAndView mv = new ModelAndView("login");
-        return mv;
-    }
 }
